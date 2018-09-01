18a0a80596199d7cc67dc7a47e8b69846426da0f
steven.anderson@adp.com <saanderson1987@gmail.com>
2018-09-01 17:13:43 -0400

    update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // module to connect to PostgreSQL DB.
 const pgp = require('pg-promise')();
-const connectionStr = 'postgres://localhost:5432/text_annotator'
+const connectionStr = process.env.DATABASE_URL || 'postgres://localhost:5432/text_annotator'
 const db = pgp(connectionStr);
 
 module.exports = db;
